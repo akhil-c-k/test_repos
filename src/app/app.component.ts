b58b99c6e321ca9b37b7d0711256ca01db315801
Akhil_ck <akhilmzh1@gmail.com>
2018-02-25 16:29:32 +0530

    first test file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+       <h1>{{title}}</h1>
+       <h2>My favorite hero is: {{myHero}}</h2>
+       <p>Heroes:</p>
+       <ul>
+        <li *ngFor="let hero of heroes"> {{ hero }}</li>
+       </ul>  
+      `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  heroes = ['Iron Man','Thor','Batman','Superman'];
+  myHero = this.heroes[0];
 }
