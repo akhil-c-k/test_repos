9bbe557061aa012abdf8543ca560a13419f0403c
Akhil_ck <akhilmzh1@gmail.com>
2018-02-25 18:47:19 +0530

    Created new class, imported inside component. working perfect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
+import {Hero} from  './hero';
 
 @Component({
   selector: 'app-root',
   template: `
-       <h1>{{title}}</h1>
-       <h2>My favorite hero is: {{myHero}}</h2>
-       <p>Heroes:</p>
-       <ul>
-        <li *ngFor="let hero of heroes"> {{ hero }}</li>
-       </ul>  
-      `
+  <h1>{{title}}</h1>
+  <h2>My favorite hero is: {{myHero.name}}</h2>
+  <p>Heroes:</p>
+  <ul>
+    <li *ngFor="let hero of heroes">
+      {{ hero.name }}
+    </li>
+  </ul>
+`
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = ['Iron Man','Thor','Batman','Superman'];
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
   myHero = this.heroes[0];
 }
